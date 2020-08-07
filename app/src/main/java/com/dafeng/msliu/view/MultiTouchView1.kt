e6a1b267384eda03f1786a167db1d3e2693a0b6e
da.feng <da.feng@17zuoye.com>
2020-08-07 08:49:10 +0800

    多指触摸

--- app/src/main/java/com/dafeng/msliu/view/MultiTouchView1.kt
+++ app/src/main/java/com/dafeng/msliu/view/MultiTouchView1.kt
@@ -5,13 +5,25 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import com.blankj.utilcode.util.ConvertUtils
 import com.dafeng.msliu.R
 import com.dafeng.msliu.Utils.Utils
 
 class MultiTouchView1 :View{
+
+    companion object{
+        @JvmStatic
+        val IMAGE_WIDTH= ConvertUtils.dp2px(200f)
+    }
     private lateinit var mBitMap: Bitmap
     private lateinit var mPaint: Paint
+    private var offsetX:Float=0f
+    private var offsetY:Float=0f
+    private var downX:Float=0f
+    private var downY:Float=0f
+
     constructor(context:Context,attrs:AttributeSet) : super(context, attrs){
          init(context);
     }
@@ -20,11 +32,29 @@ class MultiTouchView1 :View{
     }
 
     private fun init(context: Context) {
-
-         mBitMap = Utils.drawableToBitmap(resources.getDrawable(R.drawable.huoying), 100, 100)
+         mBitMap = Utils.drawableToBitmap(resources.getDrawable(R.drawable.huoying), IMAGE_WIDTH, IMAGE_WIDTH)
     }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when(event.actionMasked){
+            MotionEvent.ACTION_DOWN->{
+                downX=event.x;
+                downY=event.y;
+            }
+            MotionEvent.ACTION_MOVE->{
+                offsetX=event.x-downX;
+                offsetY=event.y-downY;
+                invalidate()
+            }
+
+        }
+
+        return true
+    }
+
+
+
     override fun onDraw(canvas: Canvas) {
-        canvas.run { drawBitmap(mBitMap,0f,0f,mPaint) }
+        canvas.run { drawBitmap(mBitMap,offsetX,offsetY,mPaint) }
     }
 }
